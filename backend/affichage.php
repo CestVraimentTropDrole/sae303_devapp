41570249617f54f192fec40b805fc6883c0e4dc3
CestVraimentTropDrole <149939786+CestVraimentTropDrole@users.noreply.github.com>
2024-12-10 09:30:33 +0100

    feat: link to database and devices view

---
+++ backend/affichage.php
@@ -0,0 +1,39 @@
+<?php
+
+//Fonction qui recherche chaque actionneur dans la base de données
+function requete_actionneurs ($db) {
+    // Requête SQL sécurisée
+    $requete = "SELECT device.id, device.nom, device.etat
+                FROM device
+                WHERE device.type = 'actionneur'";
+
+    $resultat = $db->prepare($requete);
+    $resultat->execute();
+    $donnees = $resultat->fetchAll(PDO::FETCH_ASSOC);
+
+    return $donnees;
+}
+
+//Fonction qui affiche chaque bloc d'actionneur dans la base de données
+function affiche_actionneurs ($nom, $etat) {
+    //Boucle qui interprète l'état de l'actionneur pour l'afficher
+    $statut = "";
+    if ($etat == 1) {
+        $statut = "actif";
+    }
+    else {
+        $statut = "inactif";
+    }
+
+    echo "<div class='aspect-square p-3 rounded-md bg-gray flex flex-col gap-6 justify-center items-center'>
+                <div class='inline-flex rounded-full bg-lightgray items-center justify-center p-4 w-fit h-fit'>
+                    <img src='../../asset/images/".$nom.".svg' alt=".$nom." class='w-[12dvw] h-[6dvh]'>
+                </div>
+                <div>
+                    <h2 class='font-montserrat text-lg font-bold text-center capitalize'>".htmlspecialchars($nom)."</h2>
+                    <p class='font-montserrat text-base text-center capitalize'>".htmlspecialchars($statut)."</p>
+                </div>
+            </div>";
+}
+
+?>
--- frontend/src/index.php
+++ frontend/src/index.php
@@ -1,59 +1,33 @@
 <?php
     include "../../backend/database.php";
+    include "../../backend/affichage.php";
 ?>
 
 <html class="w-full h-full">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="output.css" rel="stylesheet">
         <title>Gère ta baraque</title>
     </head>
+
     <body class="w-full h-full flex flex-col gap-4 p-6">
         <header class="w-full h-fit border-solid border-red border-b-2">
             <h1 class="text-center font-bold text-xl pb-3 text-white">Domotique</h1>
         </header>
 
         <div class="grid grid-cols-2 gap-3">
-            <div class="aspect-square p-3 rounded-md bg-gray flex flex-col gap-6 justify-center items-center">
-                <div class="inline-flex rounded-full bg-lightgray items-center justify-center p-4 w-fit h-fit">
-                    <img src="../../asset/images/Radiator.svg" alt="Chauffage" class="w-[12dvw] h-[6dvh]">
-                </div>
-                <div>
-                    <h2 class="font-montserrat text-lg font-bold text-center">Chauffage</h2>
-                    <p class="font-montserrat text-base text-center">Statut</p>
-                </div>
-            </div>
 
-            <div class="aspect-square p-3 rounded-md bg-gray flex flex-col gap-6 justify-center items-center">
-                <div class="inline-flex rounded-full bg-lightgray items-center justify-center p-4 w-fit h-fit">
-                    <img src="../../asset/images/Ampoule.svg" alt="Lumière" class="w-[12dvw] h-[6dvh]">
-                </div>
-                <div>
-                    <h2 class="font-montserrat text-lg font-bold text-center">Lumière</h2>
-                    <p class="font-montserrat text-base text-center">Statut</p>
-                </div>
-            </div>
+            <?php
+                $donnees = requete_actionneurs($db); //On récupère les données des actionneurs
 
-            <div class="aspect-square p-3 rounded-md bg-gray flex flex-col gap-6 justify-center items-center">
-                <div class="inline-flex rounded-full bg-lightgray items-center justify-center p-4 w-fit h-fit">
-                    <img src="../../asset/images/Volets.svg" alt="Volets" class="w-[12dvw] h-[6dvh]">
-                </div>
-                <div>
-                    <h2 class="font-montserrat text-lg font-bold text-center">Volets</h2>
-                    <p class="font-montserrat text-base text-center">Statut</p>
-                </div>
-            </div>
-
-            <div class="aspect-square p-3 rounded-md bg-gray flex flex-col gap-6 justify-center items-center">
-                <div class="inline-flex rounded-full bg-lightgray items-center justify-center p-4 w-fit h-fit">
-                    <img src="../../asset/images/Store.svg" alt="Store" class="w-[12dvw] h-[6dvh]">
-                </div>
-                <div>
-                    <h2 class="font-montserrat text-lg font-bold text-center">Store</h2>
-                    <p class="font-montserrat text-base text-center">Statut</p>
-                </div>
-            </div>
+                foreach ($donnees as $ligne) { //On rentre dans une boucle pour afficher chaque actionneur
+                    affiche_actionneurs($ligne['nom'], $ligne['etat']);
+                }
+            ?>
         </div>
+
     </body>
+    
 </html>
